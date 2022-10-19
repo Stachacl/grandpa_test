1599255092f805cbea88c0f736445dc0ee2511ef
Stachacl <stacha.l@icloud.com>
2022-10-20 00:32:27 +1100

    Navbar links and icons added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React from "react";
 
 export const Navbar: React.FunctionComponent = () => (
-    <nav> 
-     <div className="nav-wrapper">
-      <a href="#" className="brand-logo">Logo</a>
-      <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
-      </ul>
-     </div>
-    </nav>
-  )
+  <nav>
+    <div className="nav-wrapper">
+      <a href="#" className="brand-logo">
+        Grandpa test
+      </a>
 
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li>
+          <a href="sass.html"> <i className="material-icons left">font_download</i>Increase font
+            size </a> 
+        </li>
+
+        <li>
+          <a href="sass.html">
+            <i className="material-icons left">volume_up</i>Read the page to me
+          </a>
+        </li>
+ 
+        <li>
+          <a href="sass.html">
+            <i className="material-icons left">email</i>Contact
+          </a>
+        </li>
+        
+      </ul>
+    </div>
+  </nav>
+);
