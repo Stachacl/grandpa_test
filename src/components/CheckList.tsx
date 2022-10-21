d71eff958c7ffcd37df820bb7b5d020fab2c53db
Stachacl <stacha.l@icloud.com>
2022-10-21 22:13:43 +1100

    checklist list element created

--- src/components/CheckList.tsx
+++ src/components/CheckList.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 export const CheckList: React.FC = () => {
   return (
-    <div>CheckList</div>
+    <ul>
+        <li className='checklist'>
+           <label>
+            <input type="checkbox" />
+            <span></span>
+            <i className='material-icons red-text'>delete</i>
+           </label>
+        </li>
+    </ul>
+    
   )
 }
 
