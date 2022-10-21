d2ed7db36217c2fd80127bf18901e3d4f53b4c6d
Stachacl <stacha.l@icloud.com>
2022-10-21 22:02:46 +1100

    CkeckListFormProps added to the component

--- src/components/CheckListForm.tsx
+++ src/components/CheckListForm.tsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
-
 //other way to do input - is to useRef hook, but I am using useState this time
 
+// interface that describes component's properties:
+interface CkeckListFormProps {
+  //i am expecting onAdd fnction with the type void - nothing to return, and it will have a title with the type sting:
+  onAdd(title:string): void
+}
+
 // <{onAdd():void}> = (props) - here I am letting TypeScript know that i am expecting onAdd void in the App.tst 
-export const CheckListForm: React.FC <{onAdd(title:string):void}> = (props) => {
+export const CheckListForm: React.FC <CkeckListFormProps> = (props) => {
   const [title, setTitle] = useState<string>("");
   
   const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
