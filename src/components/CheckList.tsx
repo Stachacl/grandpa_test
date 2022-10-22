c2805168197a6344763a40c2e24d9bab1ddc01fe
Stachacl <stacha.l@icloud.com>
2022-10-22 21:48:03 +1100

    described methods on change for Checkbox and onclick for bin

--- src/components/CheckList.tsx
+++ src/components/CheckList.tsx
@@ -1,37 +1,43 @@
-import React from 'react'
-import { ICheckList } from '../interfaces'
+import React from "react";
+import { ICheckList } from "../interfaces";
 
 type CheckListProps = {
-    checklist: ICheckList[]
-    //parameters to handle checkbox and Bin icon 
-    onCheckBox(id: number): void
-    onBin(id: number): void
-}
+  checklist: ICheckList[];
+  //parameters to handle checkbox and Bin icon
+  onCheckBox(id: number): void;
+  onBin(id: number): void;
+};
 
-export const CheckList: React.FC<CheckListProps> = ({ checklist, onCheckBox, onBin }) => {
+export const CheckList: React.FC<CheckListProps> = ({
+  checklist,
+  onCheckBox,
+  onBin,
+}) => {
   return (
     <ul>
-        {checklist.map(checkListItem => {
+      {checklist.map((checkListItem) => {
+        const classes = ["checkListItem"];
+        if (checkListItem.completed) {
+          classes.push("completed");
+        }
 
-            const classes = ['checkListItem']
-            if (checkListItem.completed) {
-                classes.push("completed")
-            }
-
-            return (
-              <li className={classes.join(' ')} key={checkListItem.id}>
-                <label>
-                 <input type="checkbox" checked={checkListItem.completed} />
-                 <span>{checkListItem.title}</span>
-                 <i className='material-icons red-text'>delete</i>
-                </label>
-              </li>
-            )
-        } )}
-       
+        return (
+          <li className={classes.join(" ")} key={checkListItem.id}>
+            <label>
+              <input type="checkbox" checked={checkListItem.completed} onChange={onCheckBox.bind(null, checkListItem.id )}/>
+              <span>{checkListItem.title}</span>
+              <i
+                className="material-icons red-text"
+                onClick={() => onBin(checkListItem.id)}
+              >
+                delete
+              </i>
+            </label>
+          </li>
+        );
+      })}
     </ul>
-    
-  )
-}
+  );
+};
 
-export default CheckList
+export default CheckList;
