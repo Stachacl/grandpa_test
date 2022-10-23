018af71bdd7d64f173ee79978795aaa98195f853
Stachacl <stacha.l@icloud.com>
2022-10-23 21:07:46 +1100

    creating routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React  from "react";
 import { Navbar } from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CheckListPage } from "./pages/CheckListPage";
+import { ContactPage } from "./pages/ContactPage";
 
 const App: React.FunctionComponent = () => {
   
@@ -7,6 +10,12 @@ const App: React.FunctionComponent = () => {
     <>
       <Navbar />
       <div className="container">
+       <BrowserRouter> 
+         <Routes>
+           <Route element={<CheckListPage/>} path='/'/>
+           <Route element={<ContactPage/>} path='/'/>
+         </Routes>
+        </BrowserRouter>
       </div>
     </>
   );
