db75fd5e33e059648d84c75e417f95b65e8aa644
Stachacl <stacha.l@icloud.com>
2022-10-24 21:59:53 +1100

    hero image added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export const Hero: React.FC = () => {
   return (
-    <div id="index-banner" className="parallax-container"> 
-    <div className="parallax"><img src="https://ik.imagekit.io/stcl/Grandpa/grandpa1_9CKLLvI8z.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1666608394593" alt="Unsplashed background img 1"/>
-    </div>
-    </div>
-  )
-}
+    <img
+      className="responsive-img"
+      src="https://ik.imagekit.io/stcl/Grandpa/grandpa1_9CKLLvI8z.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1666608394593"
+      alt="Grandpa is testing your website on his laptop, sitting on the couch"
+    />
+  );
+};
