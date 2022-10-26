8a13fa98511a86d2cd2ebfeefdb1a1fca7a4eaa2
Stachacl <stacha.l@icloud.com>
2022-10-26 18:33:19 +1100

    declared lost module - react-font-size-changer - fixed the bug with the fint size button

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { CheckListPage } from "./pages/CheckListPage";
 import { ContactPage } from "./pages/ContactPage";                                   
 import { Footer } from "./components/Footer";
 import { Hero } from "./components/Hero";
+import FontSizeChanger from "react-font-size-changer";
 
 const App: React.FunctionComponent = () => {
   return (
@@ -19,6 +20,35 @@ const App: React.FunctionComponent = () => {
           </Routes>
         </div>
         <Footer />
+
+        <div className="app">
+        <FontSizeChanger
+          targets={['#target .content']}
+          onChange={() => {
+            console.log();
+          }}
+          options={{
+            stepSize: 2,
+            range: 3
+          }}
+          customButtons={{
+            up: <span style={{'fontSize': '36px'}}>A</span>,
+            down: <span style={{'fontSize': '20px'}}>A</span>,
+            style: {
+              backgroundColor: 'red',
+              color: 'white',
+              WebkitBoxSizing: 'border-box',
+              WebkitBorderRadius: '5px',
+              width: '60px'
+            },
+            buttonsMargin: 10
+          }}          
+        />
+        <div id="target">
+          <p className="title">This is the title of my target text</p>
+          <p className="content">This is the content of my target text</p>
+        </div>
+      </div>
       </BrowserRouter>
     </>
   );
