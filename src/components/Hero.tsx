2d8177bbf510e512de3658e3ebc69acf63fa2280
Stachacl <stacha.l@icloud.com>
2022-10-26 21:55:26 +1100

    hero image adjusted and styles

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,33 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import '../index.css'
+import "../index.css";
 import FontSizeChanger from "react-font-size-changer";
-
 
 export const Hero: React.FC = () => {
   return (
-    <div>
-      <div className="hero">
+    <div className="hero">
+      <div className="container">
         <h1> My Grandpa will test your website</h1>
-
         <FontSizeChanger
-            targets={["#target .content"]}
-            // onChange={() => {
-            //   console.log();
-            // }}
-            options={{
-              stepSize: 2,
-              range: 3,
-            }}
-            // customButtons={{
-            //   up: <i className="small material-icons ">font_download</i>,
-            //   down: <i className="tiny material-icons ">font_download</i>,
-            // }}
-          />
+          targets={["#target .content"]}
+          options={{
+            stepSize: 2,
+            range: 4,
+          }}
+          // customButtons={{
+          //   up: <i className="small material-icons ">font_download</i>,
+          //   down: <i className="tiny material-icons ">font_download</i>,
+          // }}
+        />
         <div id="target">
-        <p className="content">
-        If your business is in a jargon-heavy industry—and you feel it helps your team iterate around common ideas successfully—experiment to be more precise about the jargon that you do use. One way to do this is to develop a set of consistent terminology within your company that everyone understands. 
-        </p>
+          <h3 className="content">
+            If your business is in a jargon-heavy industry—and you feel it helps
+            your team iterate around common ideas successfully—experiment to be
+            more precise about the jargon that you do use. One way to do this is
+            to develop a set of consistent terminology within your company that
+            everyone understands.
+          </h3>
         </div>
 
         <div>
@@ -39,7 +38,6 @@ export const Hero: React.FC = () => {
           </Link>
         </div>
       </div>
-     
     </div>
   );
 };
