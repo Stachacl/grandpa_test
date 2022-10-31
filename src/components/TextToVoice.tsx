4336a332d91eb248e0d6d1de0ad342c2bfa7f643
Stachacl <stacha.l@icloud.com>
2022-10-31 13:08:40 +1100

    text to voice function is now working - bug fixed

--- src/components/TextToVoice.tsx
+++ src/components/TextToVoice.tsx
@@ -1,38 +1,40 @@
 import React from "react";
+import { useState } from "react";
 import { getSpeech } from "voice-rss-client/dist/browser";
 
 export const TextToVoice: React.FC = () => {
 
-  let myAudioElement = '';
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    
-    const downloadedSpeech = getSpeech({
-        apiKey: "1dc335953c2b420db1f93a74eab42147",
-        language: "en-us",
-        text: "foo You can import the library foo",
-      });
+  const [myAudioElement, setMyAudioElement] = useState({});
 
-      
-      downloadedSpeech.then(value => {
-        myAudioElement = value.replace('data:', 'data:audio/wav');
-        console.log(myAudioElement);
-      });
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+
+    const downloadedSpeech = getSpeech({
+      apiKey: "1dc335953c2b420db1f93a74eab42147",
+      language: "en-us",
+      text: "the voice is working",
+    });
+
+    downloadedSpeech.then((value) => {
+      const downloadedAudioElement = value.replace("data:", "data:audio/wav");
+      setMyAudioElement(downloadedAudioElement);
+    });
   };
 
   return (
-    <div>
+    <>
       <div>
-        <audio controls src={myAudioElement}></audio>
-        <button onClick={handleClick}>Click to read</button>
+        <div>
+          <audio controls src={myAudioElement}></audio>
+          <button onClick={handleClick}>Click to read</button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
 export default TextToVoice;
 
-
 // The code above is a combination of these two:
 // https://www.voicerss.org/api/
-// and 
+// and
 // https://github.com/petarvujovic98/voice-rss-client
