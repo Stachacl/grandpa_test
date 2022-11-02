1449bbc432b69968f480d6c24e92d79d59ac9df4
Stachacl <stacha.l@icloud.com>
2022-11-02 21:45:09 +1100

    footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 export const Footer: React.FunctionComponent = () => (
   <footer className="page-footer deep-orange darken-4">
-    <div className="container">
+    <div className="container m1">
       <div className="row">
         <div className="col l6 s12">
           <h5 className="white-text">Want to learn more?</h5>
@@ -13,17 +13,17 @@ export const Footer: React.FunctionComponent = () => (
               className="brown-text text-lighten-3"
               href="http://materializecss.com"
             >
-              Stacha Clemens.
+              Stacha Clemens 
             </a>
-             She is a front-end developer and a acessibility advocate. Read her
-            blogpost on Medium or connect with her via Twitter.
+            . She is a front-end developer and an acessibility advocate. Read her
+            blogposts on Medium or connect with her via Twitter.
           </p>
         </div>
       
       </div>
     </div>
     <div className="footer-copyright">
-      <div className="container">
+      <div className="container ml3">
       © Stacha Clemens 2022
       </div>
     </div>
